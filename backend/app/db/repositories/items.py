1134d62cc3301e26166f852d0543b103f61d84e3
ramondelafuente <ramon@future500.nl>
2022-08-18 01:22:36 +0200

    Add a search filter for title

--- backend/app/db/repositories/items.py
+++ backend/app/db/repositories/items.py
@@ -103,6 +103,7 @@ class ItemsRepository(BaseRepository):  # noqa: WPS214
     async def filter_items(  # noqa: WPS211
         self,
         *,
+        title: Optional[str] = None,
         tag: Optional[str] = None,
         seller: Optional[str] = None,
         favorited: Optional[str] = None,
@@ -135,6 +136,18 @@ class ItemsRepository(BaseRepository):  # noqa: WPS214
                 SELLER_USERNAME_ALIAS,
             ),
         )
+        # fmt: on
+
+        if title:
+            query_params.append('%' + str(title) + '%')
+            query_params_count += 1
+
+            # fmt: off
+            query = query.where (
+                items.title.like(Parameter(query_params_count))
+            )
+            # fmt: on
+
         # fmt: on
 
         if tag:
